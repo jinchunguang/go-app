0086624aa68990aa09bcb9512754e412f4b22374
jinchunguang <jinchunguang@jiayunhui.com>
2019-06-30 15:36:55 +0800

    update files

---
+++ deploy-server/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"os/exec"
+)
+
+
+func Run(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w,"<h1>Deploy running </h1>")
+
+	cmd:=exec.Command("sh","./deploy.sh")
+	err := cmd.Start()
+	if err!=nil{
+		log.Fatalln("cmd  start error")
+	}
+
+
+	io.WriteString(w,"<h1>Deploy ending </h1>")
+	cmd.Wait()
+
+}
+
+func Ping(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w,"<h1>Deploy pong</h1>")
+}
+
+
+func main() {
+	http.HandleFunc("/", Ping)
+	http.HandleFunc("/run", Run)
+	http.ListenAndServe(":5000", nil)
+}
---
+++ web-server/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+
+func DefaultPage(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w,"<h1>Hello World </h1>")
+}
+
+// GOOS=linux GOARCH=amd64 go build -o deploy-web main.go
+
+func main() {
+	http.HandleFunc("/", DefaultPage)
+	http.ListenAndServe(":9000", nil)
+}
